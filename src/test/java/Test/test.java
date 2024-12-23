7b865ef7ffc94c02e85b9af931d2226d158ac51e
mert.yeni <mertyeni@outlook.com.tr>
2024-12-24 01:53:42 +0300

    FlightScenarioUpdate Ferry Otel BusUpdate

--- src/test/java/Test/test.java
+++ src/test/java/Test/test.java
@@ -2,21 +2,21 @@ package Test;
 
 import method.BaseMethod;
 import org.openqa.selenium.JavascriptExecutor;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.awt.dnd.Autoscroll;
+import java.util.Iterator;
+import java.util.Set;
 
 import static contant.BaseContant.*;
 
 public class test extends BaseMethod {
 
-@BeforeMethod
-public void beforeMethod() {
-    setUp();
-}
+
+    @BeforeMethod
+    public void beforeMethod() {
+        setUp();
+    }
 
     @Test // Flight - Funnel
     public void testFlight() throws InterruptedException {
@@ -30,7 +30,7 @@ public void beforeMethod() {
         click(DATE_DAY);
         Thread.sleep(4000);
         click(SEARCH_BUTTON);
-        Thread.sleep(15000);
+        Thread.sleep(8000);
         click(POPUP_CLOSE);
         Thread.sleep(2000);
         js.executeScript("window.scrollTo(0, 800);");
@@ -39,13 +39,13 @@ public void beforeMethod() {
         Thread.sleep(2000);
         click(FARELIST);
         Thread.sleep(8000);
-        sendKeys(INPUTEMAIL,"mert.yeni@obilet.com");
+        sendKeys(INPUTEMAIL, "mert.yeni@obilet.com");
         Thread.sleep(1000);
-        sendKeys(INPUTPHONE,"5063637195");
+        sendKeys(INPUTPHONE, "5063637195");
         Thread.sleep(1000);
-        sendKeys(INPUTFIRSTNAME,"Mert");
+        sendKeys(INPUTFIRSTNAME, "Mert");
         Thread.sleep(1000);
-        sendKeys(INPUTLASTNAME,"Yeni");
+        sendKeys(INPUTLASTNAME, "Yeni");
         Thread.sleep(1000);
         click(INPUTBIRTHDAY);
         Thread.sleep(1200);
@@ -65,37 +65,34 @@ public void beforeMethod() {
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
         Thread.sleep(2000);
         // Sayfayı geri yukarıya kaydırma
-        js.executeScript("window.scrollTo(0, 0);");
+        js.executeScript("window.scrollTo(0, 0);"); //
         Thread.sleep(1000);
-        sendKeys(INPUTCARDNUMBER,"4111");
-        sendKeys(INPUTCARDNUMBER,"1111");
-        sendKeys(INPUTCARDNUMBER,"1111");
-        sendKeys(INPUTCARDNUMBER,"1111");
+        sendKeys(INPUTCARDNUMBER, "4111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
         Thread.sleep(1000);
-        sendKeys(INPUTCARDEXP,"01");
-        sendKeys(INPUTCARDEXP,"25");
+        sendKeys(INPUTCARDEXP, "01");
+        sendKeys(INPUTCARDEXP, "25");
         Thread.sleep(1000);
-        sendKeys(INPUTCARDCVC,"001");
+        sendKeys(INPUTCARDCVC, "001");
         Thread.sleep(1000);
-
-        //click(INSTALLEMENTOPTIONS);
-        //Thread.sleep(1000);
-        //click(INSTALLEMENT);
-
-
 
     }
 
-    @Test  // BusTR - Funnel
+
+
+
+    @Test // BusTR - Funnel
     public void testBus() throws InterruptedException {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         click(FIRSTALLOW);
         Thread.sleep(1000);
-        sendKeys(ORIGIN_BUTTON,"Bucak");
+        sendKeys(ORIGIN_BUTTON, "Bucak");
         Thread.sleep(3000);
         click(BUSORIGINID);
         Thread.sleep(1000);
-        sendKeys(DESTINATION_BUTTON,"İzmir");
+        sendKeys(DESTINATION_BUTTON, "İzmir");
         Thread.sleep(3000);
         click(BUSDESTINATIONID);
         Thread.sleep(1000);
@@ -104,21 +101,192 @@ public void beforeMethod() {
         click(BUSDATEDAY);
         Thread.sleep(4000);
         click(SEARCH_BUTTON);
-        Thread.sleep(8000);
+        Thread.sleep(10000);
         js.executeScript("window.scrollTo(0, 300);");
         Thread.sleep(1500);
         click(BUSDETAILS);
-        /*Thread.sleep(5500);
+        Thread.sleep(5500);
         click(CHOOSESEAT);
         Thread.sleep(2000);
         click(CHOOSEGENDER);
         Thread.sleep(2000);
         click(CONFIRMJOURNEY);
-        Thread.sleep(10000);
-        */
+        Thread.sleep(6000);
+        sendKeys(INPUTEMAIL, "mert.yeni@obilet.com");
+        Thread.sleep(1000);
+        sendKeys(INPUTPHONE, "5063637195");
+        Thread.sleep(1000);
+        sendKeys(CHECKOUTNAME, "Mert Yeni");
+        Thread.sleep(1000);
+        sendKeys(CHECKOUTNATIONALID, "55972064536");
+        Thread.sleep(1000);
+        sendKeys(INPUTCARDNUMBER, "4111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        Thread.sleep(1000);
+        sendKeys(INPUTCARDEXP, "01");
+        sendKeys(INPUTCARDEXP, "25");
+        Thread.sleep(1000);
+        sendKeys(INPUTCARDCVC, "001");
+        Thread.sleep(1000);
+
     }
+
+
+    @Test // Otel Funnel
+    public void testOtel() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        click(OTELBUTTON);
+        Thread.sleep(1000);
+        click(FIRSTALLOW);
+        Thread.sleep(1000);
+        click(ORIGIN_BUTTON);
+        Thread.sleep(3000);
+        click(OTELORIGINID);
+        Thread.sleep(1000);
+        click(OTELDEPARTUREINPUT);
+        Thread.sleep(1000);
+        click(OTELENTERDATE);
+        Thread.sleep(1000);
+        click(OTELOUTDATE);
+        Thread.sleep(1000);
+        click(PAXTOGGLE);
+        Thread.sleep(1000);
+        click(REMOVETOGGLE);
+        Thread.sleep(1000);
+        click(SEARCH_BUTTON);
+        Thread.sleep(10000);
+        js.executeScript("window.scrollTo(0, 150);");
+        click(QUICKFILTER1);
+        Thread.sleep(1000);
+        click(OTELSELECT);
+        Thread.sleep(8000);
+        Set<String> windowHandles = driver.getWindowHandles();
+        Iterator<String> iterator = windowHandles.iterator();
+        String firstWindowHandle = iterator.next();
+        String secondWindowHandle = iterator.next();
+        driver.switchTo().window(secondWindowHandle);
+        Thread.sleep(1000);
+        click(OTELPOPUPCLOSE);
+        Thread.sleep(2000);
+        js.executeScript("window.scrollTo(0, 600);");
+        Thread.sleep(1500);
+        click(OTELGOTOROOM);
+        Thread.sleep(12000);
+        sendKeys(INPUTEMAIL, "mert.yeni@obilet.com");
+        Thread.sleep(1000);
+        sendKeys(INPUTPHONE, "5063637195");
+        Thread.sleep(1000);
+        click(SMSAPPROVE);
+        Thread.sleep(1000);
+        // Sayfayı en alta kaydırma işlemi
+        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        Thread.sleep(1500);
+        sendKeys(OTELFIRSTNAME, "Mert");
+        Thread.sleep(1000);
+        sendKeys(OTELLASTNAME, "Yeni");
+        Thread.sleep(1000);
+        sendKeys(OTELNATIONALID, "55972064536");
+        Thread.sleep(1000);
+        click(OTELGENDERCHECK);
+        Thread.sleep(1000);
+        // Sayfayı geri yukarıya kaydırma
+        js.executeScript("window.scrollTo(0, 0);"); //
+        Thread.sleep(1500);
+        sendKeys(INPUTCARDNUMBER, "4111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        sendKeys(INPUTCARDNUMBER, "1111");
+        Thread.sleep(1000);
+        sendKeys(INPUTCARDEXP, "01");
+        sendKeys(INPUTCARDEXP, "25");
+        Thread.sleep(1000);
+        sendKeys(INPUTCARDCVC, "001");
+        Thread.sleep(1000);
+
+    }
+
+
+    @Test // Ferry funnel
+        public void testFerry() throws InterruptedException {
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            click(FERRYBUTTON);
+            Thread.sleep(1000);
+            click(FIRSTALLOW);
+            Thread.sleep(1000);
+            click(DATE_BUTTON);
+            Thread.sleep(2000);
+            click(DATE_DAY);
+            Thread.sleep(4000);
+            click(SEARCH_BUTTON);
+            Thread.sleep(8000);
+            js.executeScript("window.scrollTo(0, 400);");
+            Thread.sleep(1500);
+            click(FERRYJOURNEYSELECT);
+            Thread.sleep(1000);
+            click(FERRYFARESELECT);
+            Thread.sleep(2200);
+            click(FERRYSEATSELECT);
+            Thread.sleep(2000);
+            click(FERRYCONFIRMJOURNEY);
+            Thread.sleep(10000);
+            sendKeys(INPUTEMAIL, "mert.yeni@obilet.com");
+            Thread.sleep(1000);
+            sendKeys(INPUTPHONE, "5063637195");
+            Thread.sleep(1000);
+            sendKeys(FERRYINPUTNAME,"Mert Yeni");
+            Thread.sleep(1000);
+            sendKeys(FERRYINPUTGOVID,"55972064536");
+            Thread.sleep(800);
+            click(FERRYBIRTHDAY);
+            Thread.sleep(1000);
+            click(FERRYBIRTHMONTH);
+            Thread.sleep(1000);
+            click(FERRYBIRTHYEAR);
+            Thread.sleep(1000);
+            sendKeys(INPUTCARDNUMBER, "4111");
+            sendKeys(INPUTCARDNUMBER, "1111");
+            sendKeys(INPUTCARDNUMBER, "1111");
+            sendKeys(INPUTCARDNUMBER, "1111");
+            Thread.sleep(1000);
+            sendKeys(INPUTCARDEXP, "01");
+            sendKeys(INPUTCARDEXP, "25");
+            Thread.sleep(1000);
+            sendKeys(INPUTCARDCVC, "001");
+            Thread.sleep(1000);
+            click(FERRYCONFIRMCOOKIE);
+            Thread.sleep(1000);
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
     @AfterMethod
     public void afterMethod() {
     tearDown();
     }
-}
+    }
+
+
--- src/test/java/contant/BaseContant.java
+++ src/test/java/contant/BaseContant.java
@@ -3,11 +3,15 @@ package contant;
 import org.openqa.selenium.By;
 
 
+
 public class BaseContant {
 
     public static final By FLIGHT_BUTTON= By.cssSelector("[data-event-action='Flight']");
     public static final By ORIGIN_BUTTON= By.id("origin-input");
     public static final By DESTINATION_BUTTON= By.id("destination-input");
+    public static final By RETURNSELECT=By.id("two-ways");
+    public static final By RETURNDATEBUTTON=By.xpath("//*[@id=\"search\"]/div[2]/div[4]");
+    public static final By RETURNDATE_DAY=By.xpath("//*[@id=\"return\"]/div[2]/div[1]/div[2]/div/table/tbody/tr[4]/td[5]/button");
     public static final By SEARCH_BUTTON= By.id("search-button");
     public static final By DATE_BUTTON= By.xpath("//*[@id=\"search\"]/div[2]/div[3]/div");
     public static final By DATE_DAY= By.xpath("//*[@id=\"departure\"]/div[2]/div[1]/div[2]/div/table/tbody/tr[1]/td[4]/button");
@@ -39,9 +43,52 @@ public class BaseContant {
     public static final By BUSDATEBUTTON=By.xpath("//*[@id=\"departure\"]/div[1]/div[1]");
     public static final By BUSDATEDAY=By.xpath("//*[@id=\"departure\"]/div[3]/div/div[2]/div/table/tbody/tr[4]/td[3]/button/span");
     public static final By BUSDETAILS=By.xpath("//*[@id=\"journey-916659744\"]/div[1]");
-    public static final By CHOOSESEAT=By.xpath("//*[@id=\"journey-916659744\"]/div[2]/div[2]/div[2]/div[1]/div[1]/svg/a[21]/use");
+    public static final By CHOOSESEAT=By.cssSelector("#journey-916659744 > div.details.details-container.row > div.journey-details-template > div.next > div.layout > div.wrapper > svg > a:nth-child(56) > text");
     public static final By CHOOSEGENDER=By.xpath("//*[@id=\"main\"]/div[11]/div/button[1]");
     public static final By CONFIRMJOURNEY=By.xpath("//*[@id=\"journey-916659744\"]/div[2]/div[2]/div[2]/div[2]/button");
+    public static final By CHECKOUTNAME=By.xpath("//*[@id=\"name-35\"]");
+    public static final By CHECKOUTNATIONALID=By.xpath("//*[@id=\"gov-id-35\"]");
+
+
+
+
+
+
+    //OTEL
+    public static final By OTELBUTTON=By.cssSelector("[data-event-action='Hotel']");
+    public static final By OTELORIGINID=By.xpath("//*[@id=\"origin\"]/div/ul[1]/li[3]");
+    public static final By OTELDEPARTUREINPUT=By.id("departure-input");
+    public static final By OTELENTERDATE=By.xpath("//*[@id=\"checkInDate\"]/div[3]/div/div[2]/table[1]/tbody[2]/tr[5]/td[3]/button");
+    public static final By OTELOUTDATE=By.xpath("//*[@id=\"checkInDate\"]/div[3]/div/div[2]/table[1]/tbody[2]/tr[5]/td[6]/button");
+    public static final By PAXTOGGLE=By.id("pax-toggle");
+    public static final By REMOVETOGGLE=By.xpath("//*[@id=\"pax-container\"]/div[2]/div[1]/div[2]/div[1]/div[1]/div[2]/button[1]");
+    public static final By QUICKFILTER1=By.xpath("//*[@id=\"quickFiltersContainer\"]/div/div[2]/button[1]");
+    public static final By OTELSELECT=By.xpath("//*[@id=\"outbound-journeys\"]/li[2]/div/div[3]");
+    public static final By OTELPOPUPCLOSE=By.cssSelector("#modal-content > div.hotel-coupon-modal__close-btn > svg > use");
+    public static final By OTELGOTOROOM=By.xpath("//*[@id=\"hotelRoomOffersTemplateContainer\"]/div/div[3]/div/div[2]/button");
+    public static final By OTELFIRSTNAME=By.id("first-name-0");
+    public static final By OTELLASTNAME=By.id("last-name-0");
+    public static final By OTELNATIONALID=By.id("gov-id-0");
+    public static final By OTELGENDERCHECK=By.xpath("//*[@id=\"male-0\"]/div");
+    public static final By CLICKCONTRATOPTION=By.xpath("//*[@id=\"contract-optin\"]/div");
+    public static final By SMSAPPROVE=By.xpath("//*[@id=\"subscription-optin\"]/div[1]");
+
+
+
+
+
+    // FERRY
+    public static final By FERRYBUTTON=By.cssSelector("[data-event-action='Sea']");
+    public static final By FERRYJOURNEYSELECT=By.xpath("//*[@id=\"journey-584117\"]/div[1]/div[2]");
+    public static final By FERRYFARESELECT=By.xpath("//*[@id=\"journey-584117\"]/div[2]/div[2]/ul/li[1]/div/div[2]");
+    public static final By FERRYSEATSELECT=By.cssSelector("#journey-584117 > div.details.details-container.row > div:nth-child(2) > div.next > div.layout > div.wrapper > div > svg > a:nth-child(177)");
+    public static final By FERRYCONFIRMJOURNEY=By.xpath("//*[@id=\"journey-584117\"]/div[2]/div[2]/div[1]/div[2]/button/span[1]");
+    public static final By FERRYINPUTNAME=By.id("name-165");
+    public static final By FERRYINPUTGOVID=By.id("gov-id-165");
+    public static final By FERRYBIRTHDAY=By.xpath("//*[@id=\"birth-day-165\"]/option[24]");
+    public static final By FERRYBIRTHMONTH=By.xpath("//*[@id=\"birth-month-165\"]/option[11]");
+    public static final By FERRYBIRTHYEAR=By.xpath("//*[@id=\"birth-year-165\"]/option[12]");
+    public static final By FERRYCONFIRMCOOKIE=By.xpath("//*[@id=\"contract-optin\"]/div");
 
 
 
